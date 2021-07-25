e94e99fca66d37802b482d5fbccb936cb3907e99
unknown <n.kakauridze9@gmail.comgit config --global user.email n.kakauridze9@gmail.comgit config --global user.name N1K0l0Z-KAKAUR1DZ3>
2021-07-25 19:43:34 +0400

    first commit

---
+++ test.js
@@ -0,0 +1,22 @@
+
+var colection = {
+    "2548": {
+        "album": "lolBTW",
+        "artist": "gey",
+        "tracks": [
+            "imFuckingTired",
+            "love is bad name"
+        ]
+    },
+
+    "2365": {
+        "XDD": "aName",
+        "nik": "g",
+        "tracks": [
+            "imFuckingTired",
+            "love is bad name"
+        ]
+    }
+}
+
+console.log(Object.values(colection)[0].tracks[0]);
