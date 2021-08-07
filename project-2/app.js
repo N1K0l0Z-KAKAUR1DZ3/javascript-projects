b9f2299bde09c160ea67a7203432ba9a4805b15d
unknown <n.kakauridze9@gmail.comgit config --global user.email n.kakauridze9@gmail.comgit config --global user.name N1K0l0Z-KAKAUR1DZ3>
2021-08-07 21:25:30 +0400

    reviews

--- project-2/app.js
+++ project-2/app.js
@@ -1,5 +1,5 @@
 let count = 0;
- const value = document.querySelector('#value');
+ const value = currentTarget.classList('#value');
  const btns = document.querySelectorAll('.btn')
 
  btns.forEach(function(btn) {
